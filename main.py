0de6e235eb00373cc99eccffc5bc1864cb83c517
TytUY123 <valerapohlebin74@gmail.com>
2025-05-25 15:44:07 +0500

    123

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello from ITStep")
+result = []
+def divider(a, b):
+    if a < b:
+        raise ValueError(f"Число a должно быть больше b")
+    if b > 100:
+        raise IndexError(f"чило b должно быть не больше 100")
+    return a/b
+data = {10: 2, 2: 5, "123": 4, 18: 0, 1: 15, 8 : 4}
+try:
+    for key in data:
+        res = divider(key, data[key])
+        result.append(res)
+except:
+    print(f"ошибка")
+
+print(result)
